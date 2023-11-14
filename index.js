9ce24f381a35d340fd414872e7a2da74b8d9ae8c
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-11-13 19:32:45 -0600

    day 1 reverse strings with built in functions

---
+++ index.js
@@ -0,0 +1,4 @@
+// Day 1 - Reverse a string with built in function 
+let str1 = 'Novak is a GREAT tennis player'
+
+console.log(str1.split('').reverse().join(''))
---
+++ index.py
@@ -0,0 +1,6 @@
+#Day 1 - Reverse a string with built in function 
+str1 = 'Novak is a GREAT tennis player'
+
+new_str = "".join(reversed(str1))
+
+print(new_str)
