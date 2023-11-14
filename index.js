5f711bf5baae3fa2505f0886a2de88297203a547
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-11-13 19:46:02 -0600

    day 2 reverse string without built in function

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 let str1 = 'Novak is a GREAT tennis player'
 
 console.log(str1.split('').reverse().join(''))
+
+// Day 2 - Reverse string without built in function 
+rev_str = ''
+
+for(i=str1.length -1; i>=0;i--){
+    rev_str += str1[i]
+}
+
+console.log(rev_str)
--- index.py
+++ index.py
@@ -4,3 +4,8 @@ str1 = 'Novak is a GREAT tennis player'
 new_str = "".join(reversed(str1))
 
 print(new_str)
+
+#Day 2 - Reverse string without built in function 
+rev_str = str1[::-1]
+
+print(rev_str)
