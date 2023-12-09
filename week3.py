0ae343f166699ac18deb0048512f32155a14a9cf
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-12-09 12:17:41 -0600

    return the longest string from a list

--- week3.py
+++ week3.py
@@ -7,4 +7,15 @@ def remove_symbols(word):
 
     return new_word
 
-print(remove_symbols('he!!o')) #he  o
+#print(remove_symbols('he!!o')) #he  o
+
+#Day 16
+#Return the longest string from a list
+test_list = ['hello', 'code', 'practice', 'fun']
+
+def length_elements(element):
+    return len(element)
+
+test_list.sort(key=length_elements)
+
+print(test_list[len(test_list)-1])
